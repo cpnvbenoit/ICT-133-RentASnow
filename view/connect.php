da63409518b60e7c1bfd33a75d30354d018e9f71
cpnvbenoit <47849605+cpnvbenoit@users.noreply.github.com>
2020-01-17 13:57:39 +0100

    style page connect done

--- view/connect.php
+++ view/connect.php
@@ -2,13 +2,24 @@
 ob_start();
 $title = "RentASnow - Connect";
 ?>
-<form action="submit">
-    <label for="email">Email</label>
-    <input id="email" type="text" placeholder="Entrer votre E-mail ici...">
-    <label for="password">Mot de passe</label>
-    <input id="password" type="text" placeholder="Entrer votre mot de passe ici...">
-    <button >Se connecter</button>
+<div >
+<form style="margin-top: 150px" action="submit">
+    <table class="table">
+        <tr>
+            <td><label for="email">Email : </label></td>
+            <td><input id="email" type="text" placeholder="Entrer votre E-mail ici..."></td>
+        </tr>
+        <tr>
+            <td><label for="password">Mot de passe : </label></td>
+            <td><input id="password" type="text" placeholder="Entrer votre mot de passe ici..."></td>
+        </tr>
+        <tr>
+            <td colspan="2"><button >Se connecter</button></td>
+            
+        </tr>
+    </table>
 </form>
+</div>
 <?php
 $content = ob_get_clean();
 require "gabarit.php";
